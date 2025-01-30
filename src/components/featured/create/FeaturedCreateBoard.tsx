84a0424bd298f26d9f93f800ae988c5bfefe44ae
dorjoo <dorjoohover@gmail.com>
2025-01-31 07:39:31 +0800

    featured der create form hiisen

--- src/components/featured/create/FeaturedCreateBoard.tsx
+++ src/components/featured/create/FeaturedCreateBoard.tsx
@@ -1,30 +1,42 @@
-import CoreBreadCrumb from '@/components/core/CoreBreadCrumb';
-import CoreTitle from '@/components/core/CoreTitle';
+import CoreBreadCrumb from "@/components/core/CoreBreadCrumb";
+import CoreTitle from "@/components/core/CoreTitle";
 import { Card, CardBody } from "@heroui/react";
-import { FunctionComponent } from 'react';
-import FeaturedForm from '../FeaturedForm';
-
+import { FunctionComponent } from "react";
+import FeaturedForm from "../FeaturedForm";
+import CoreSelectSupplier from "@/components/core/CoreSelectSupplier";
+import { createFeatured, getProduct, getProducts } from "@/services";
+import { ProductType } from "@/types";
+import FeaturedCreateBoardFilter from "../detail/FeaturedCreateBoardFilter";
+import { getBrandsAction } from "@/app/actions/brand";
 
 interface FeaturedCreateBoardProps {
   supplierId: string;
 }
 
-const FeaturedCreateBoard: FunctionComponent<FeaturedCreateBoardProps> = ({ supplierId }) => {
+const FeaturedCreateBoard: FunctionComponent<
+  FeaturedCreateBoardProps
+> = async ({ supplierId }) => {
+  // const { items, hasMore, isLoading, onLoadMore, onSearchValue } = useSuppliers(
+  //   { fetchDelay: 1500 }
+  // );
+
   return (
-    <div className='flex flex-col gap-4'>
-      <div className='flex justify-between gap-4 items-center'>
-        <CoreTitle text='Онцлох үүсгэх' />
+    <div className="flex flex-col gap-4">
+      <div className="flex justify-between gap-4 items-center">
+        <CoreTitle text="Онцлох үүсгэх" />
+        <FeaturedCreateBoardFilter
+          supplierId={supplierId}
+  
+        />
         <CoreBreadCrumb />
       </div>
 
       <Card>
         <CardBody>
-          <FeaturedForm supplierId={supplierId}/>
+          <FeaturedForm supplierId={supplierId} />
         </CardBody>
       </Card>
     </div>
-
-     
   );
 };
 
--- src/services/featured.services.ts
+++ src/services/featured.services.ts
@@ -1,24 +1,44 @@
-import { apiService } from '@/services';
-import { ApiResponseType, FeaturedType, SearchParamsFilterType } from '@/types';
+import { apiService } from "@/services";
+import { ApiResponseType, FeaturedType, SearchParamsFilterType } from "@/types";
 
-export async function getFeaturedList(filter: SearchParamsFilterType): Promise<ApiResponseType<FeaturedType[]>> {
-  return apiService.getList<FeaturedType[]>('/marketing/bo/promoted-items/list', filter);
+export async function getFeaturedList(
+  filter: SearchParamsFilterType
+): Promise<ApiResponseType<FeaturedType[]>> {
+  return apiService.getList<FeaturedType[]>(
+    "/marketing/bo/promoted-items/list",
+    filter
+  );
 }
 
 export async function getFeaturedOne<FeaturedType>(id: string) {
-  const res = await apiService.getOne<FeaturedType>(`/marketing/bo/promoted-items/list`, { id });
+  const res = await apiService.getOne<FeaturedType>(
+    `/marketing/bo/promoted-items/list`,
+    { id }
+  );
   return res.data;
 }
 
 export async function createFeatured<FeaturedType>(data: FeaturedType) {
-  const res = await apiService.create<FeaturedType, FeaturedType >(`/marketing/bo/promoted-items/`, data);
+  const res = await apiService.create<FeaturedType, FeaturedType>(
+    `/marketing/bo/promoted-items/`,
+    data
+  );
   return res.data;
 }
 
 export async function deleteFeatured<FeaturedType>(id: string) {
-  return await apiService.delete<FeaturedType>(`/marketing/bo/promoted-items/`, { id });
+  return await apiService.delete<FeaturedType>(
+    `/marketing/bo/promoted-items/`,
+    { id }
+  );
 }
 
-export async function updateFeatured<FeaturedType>(data: FeaturedType, id: string) {
-  return await apiService.update<FeaturedType,  FeaturedType>(`/marketing/bo/promoted-items/`, { id: id, data: data });
+export async function updateFeatured<FeaturedType>(
+  data: FeaturedType,
+  id: string
+) {
+  return await apiService.update<FeaturedType, FeaturedType>(
+    `/marketing/bo/promoted-items/`,
+    { id: id, data: data }
+  );
 }
